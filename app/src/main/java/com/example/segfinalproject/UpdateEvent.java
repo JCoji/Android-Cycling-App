b08c082dfce579b4b85100783fe83f638730fc0c
Jonathan Cojita <jcoji022@uottawa.ca>
2023-11-10 23:07:38 -0500

    Added event update function

---
+++ app/src/main/java/com/example/segfinalproject/UpdateEvent.java
@@ -0,0 +1,14 @@
+package com.example.segfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_event);
+    }
+}
